80f160e86f0cd472141d640dac191b44141b7abc
AntonShiva <ryashinanthon@yandex.ru>
2023-10-09 20:03:47 +0300

    Initial Commit

---
+++ Weather Foracase°/Weather_Foracase_App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_Foracase_App.swift
+//  Weather Foracase°
+//
+//  Created by Anton Rasen on 09.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_Foracase_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
