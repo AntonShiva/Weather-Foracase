d43bdabe8d0d70cc32401ec510efd88a138cd847
AntonShiva <ryashinanthon@yandex.ru>
2023-10-09 21:12:12 +0300

    Добавли в HomeView фоновй цвет  и изображение и изображение дома

---
+++ Weather Foracase°/Views/Main/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeVeiw.swift
+//  Weather Foracase°
+//
+//  Created by Anton Rasen on 09.10.2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
