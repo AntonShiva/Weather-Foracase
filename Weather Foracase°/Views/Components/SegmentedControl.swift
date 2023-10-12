ff780718c12fd3c4ac2deea1c75e8362eaa2184e
AntonShiva <ryashinanthon@yandex.ru>
2023-10-12 10:33:42 +0300

    создал разделительную полосу Separator и подсветку белым выделяя активный элемент SegmentedControl

--- Weather Foracase°/Views/Components/SegmentedControl.swift
+++ Weather Foracase°/Views/Components/SegmentedControl.swift
@@ -8,27 +8,50 @@
 import SwiftUI
 
 struct SegmentedControl: View {
+    @Binding var selection: Int
     var body: some View {
         VStack(spacing: 5) {
             // MARK: Segmented Buttons
             HStack {
                 Button {
-                    
+                    selection = 0
                 } label: {
                     Text("Hourly Forecast")
                 }
+                .frame(minWidth: 0, maxWidth: .infinity)
                 
                 Button {
-                    
+                    selection = 1
                 } label: {
-                    Text("Hourly Forecast")
+                    Text("Weekly Forecast")
                 }
+                .frame(minWidth: 0, maxWidth: .infinity)
             }
+            .font(.subheadline.weight(.semibold))
+            .foregroundColor(.secondary)
+            
+            // MARK: Separator
+            Divider()
+                .background(.white.opacity(0.5))
+                .blendMode(.overlay)
+                .shadow(color: .black.opacity(0.2), radius: 0, x: 0, y: 1 )
+                .blendMode(.overlay)
+                .overlay {
+                    // MARK: Underline
+                    HStack {
+                        Divider()
+                            .frame(width: UIScreen.main.bounds.width / 2, height: 3)
+                            .background(Color.underline)
+                        .blendMode(.overlay)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .offset(y: -1)
+                }
         }
         .padding(.top, 25)
     }
 }
 
 #Preview {
-    SegmentedControl()
+    SegmentedControl(selection: .constant(0))
 }
